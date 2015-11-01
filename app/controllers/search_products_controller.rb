e82a8b32c7b5eaaa0849eb210fc5c6c84aa03535
pastore27 <hirataku1988@gmail.com>
2015-11-01 11:26:54 +0900

    display title & price

--- app/controllers/search_products_controller.rb
+++ app/controllers/search_products_controller.rb
@@ -7,11 +7,28 @@ class SearchProductsController < ApplicationController
   def get_products
     res = Amazon::Ecs.item_search(
       params[:keyword],
-      :search_index => params[:category],
-      :country      => 'jp'
+      :search_index   => params[:category],
+      :response_group => 'Large',
+      :country        => 'jp'
     )
 
     puts res.first_item
+
+    @items = []
+    res.items.each do |item|
+      item_attributes = item.get_element('ItemAttributes')
+
+      title = item_attributes.get('Title')
+      price = item_attributes.get('ListPrice/Amount')
+
+      url = item.get('DetailPageURL')
+
+      puts title, price, url
+
+      @items.push({ 'title' => title, 'price' => price, 'url' => url })
+    end
+
+    puts @items
   end
 
 end
