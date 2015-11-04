71e0e2b9d8ee52a1f845bf0916f9bad1706b0872
pastore27 <hirataku1988@gmail.com>
2015-11-05 01:28:04 +0900

    extract method

--- app/controllers/search_products_controller.rb
+++ app/controllers/search_products_controller.rb
@@ -7,62 +7,27 @@ class SearchProductsController < ApplicationController
 
   def get_products
 
-    search_word    = ''
-    keyword        = params[:keyword]
-    negative_match = params[:negative_match]
-
-    keyword.split.each do |word|
-      search_word << word
-    end
-    negative_match.split.each do |word|
-      search_word << " -#{word}"
-    end
-
-    res = Amazon::Ecs.item_search(
-      search_word,
-      :search_index   => params[:category],
-      :response_group => 'Large',
-      :country        => 'jp',
-      :item_page      => params[:page]
-    )
-
-    @items = []
-    res.items.each do |item|
-      item_attributes = item.get_element('ItemAttributes')
-
-      title    = item_attributes.get('Title')
-      price    = item_attributes.get('ListPrice/Amount')
-      headline = item_attributes.get('Brand')
-
-      url = item.get('DetailPageURL')
-
-      features = item_attributes.get_elements('Feature')
-
-      @items.push({
-                    'title'    => title,
-                    'url'      => url,
-                    'price'    => price,
-                    'headline' => headline
-                  })
-    end
-
     # search_index=Allの時は5ページまでしか取得できない。Amazon APIの仕様
     max_page = params[:category] == "All" ? 5 : 10;
-    @page = {
-      'last_page'    => res.total_pages > 10 ? max_page : res.total_pages,
-      'current_page' => params[:page].to_i
-    }
 
     @search_info = {
-      'keyword'        => params[:keyword],
-      'negative_match' => params[:negative_match],
-      'category'       => params[:category],
-      'is_prime'       => params[:is_prime].to_i,
-      'page'           => params[:page],
-      'item_total'     => res.total_results,
+      'keyword'        => params['keyword'],
+      'negative_match' => params['negative_match'],
+      'category'       => params['category'],
+      'is_prime'       => params['is_prime'].to_i,
+      'page'           => params['page'],
       'max_export'     => max_page * 10
     }
 
+    # APIリクエスト
+    (res, @items) = req_api(@search_info)
+
+    @search_info['item_total'] = res.total_results
+
+    @page = {
+      'last_page'    => res.total_pages > 10 ? max_page : res.total_pages,
+      'current_page' => params['page'].to_i
+    }
     @labels = Label.all
   end
 
@@ -79,4 +44,44 @@ class SearchProductsController < ApplicationController
     redirect_to :action => 'show'
   end
 
+  def req_api(condition)
+    search_word    = ''
+    keyword        = condition['keyword']
+    negative_match = condition['negative_match']
+
+    keyword.split.each do |word|
+      search_word << " #{word}"
+    end
+    negative_match.split.each do |word|
+      search_word << " -#{word}"
+    end
+
+    res = Amazon::Ecs.item_search(
+      search_word,
+      :search_index   => condition['category'],
+      :response_group => 'Large',
+      :country        => 'jp',
+      :item_page      => condition['page']
+    )
+
+    items = []
+    res.items.each do |item|
+      item_attributes = item.get_element('ItemAttributes')
+
+      title    = item_attributes.get('Title')
+      price    = item_attributes.get('ListPrice/Amount')
+      headline = item_attributes.get('Brand')
+      url      = item.get('DetailPageURL')
+      features = item_attributes.get_elements('Feature')
+
+      items.push({
+                   'title'    => title,
+                   'url'      => url,
+                   'price'    => price,
+                   'headline' => headline
+                 })
+    end
+
+    return res, items
+  end
 end
