fc1bead9964e9ba2ec7ca024a2f8e6eef591f8b0
pastore27 <hirataku1988@gmail.com>
2015-11-05 01:11:02 +0900

    % bundle exec rake db:migrate

--- db/migrate/20151104140401_create_items.rb
+++ db/migrate/20151104140401_create_items.rb
@@ -1,6 +1,8 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
+      t.integer :label_id, :null => false
+      t.string  :asin, :null => false
 
       t.timestamps
     end
