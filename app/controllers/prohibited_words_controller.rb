7590bedcdcee01ac78c43cd41a26f3876359b79b
pastore27 <hirataku1988@gmail.com>
2015-11-28 18:52:25 +0900

    add prohibited_words management

--- app/controllers/prohibited_words_controller.rb
+++ app/controllers/prohibited_words_controller.rb
@@ -8,4 +8,24 @@ class ProhibitedWordsController < ApplicationController
     @prohibited_words = ProhibitedWord.all
   end
 
+  def create_form
+
+  end
+
+  def create
+    prohibited_word = ProhibitedWord.new(
+      :name => params['name']
+    )
+    prohibited_word.save
+
+    redirect_to :action => 'show'
+  end
+
+  def delete
+    prohibited_word = ProhibitedWord.find_by(id: params[:id])
+    prohibited_word.destroy if prohibited_word.present?
+
+    redirect_to :action => 'show'
+  end
+
 end
