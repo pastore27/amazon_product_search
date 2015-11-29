81a6c84f7dbe1977acfb27a961212650da4b693c
pastore27 <hirataku1988@gmail.com>
2015-11-29 15:33:19 +0900

    % bundle exec rake db:migrate

--- db/migrate/20151129062302_fix_table_relation.rb
+++ db/migrate/20151129062302_fix_table_relation.rb
@@ -1,4 +1,16 @@
+# coding: utf-8
 class FixTableRelation < ActiveRecord::Migration
   def change
+    # usersとlabelsの紐付け(1対多)
+    remove_column :labels, :user_id, :integer
+    add_reference :labels, :user, :after => :id, :null => false, index: true, foreign_key: true
+
+    # labelsとsearch_conditionsの紐付け(1対多)
+    remove_column :search_conditions, :label_id, :integer
+    add_reference :search_conditions, :label, :after => :id, :null => false, index: true, foreign_key: true
+
+    # search_conditionsとitemsの紐付け(1対多)
+    remove_column :items, :label_id, :integer
+    add_reference :items, :search_condition, :after => :user_id, :null => false, index: true, foreign_key: true
   end
 end
