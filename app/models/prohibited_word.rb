2b24cb5aee6e325f0884463b66830d94d7645cb8
pastore27 <hirataku1988@gmail.com>
2015-11-29 16:16:29 +0900

    add relation to prohibited_words

--- app/models/prohibited_word.rb
+++ app/models/prohibited_word.rb
@@ -1,2 +1,3 @@
 class ProhibitedWord < ActiveRecord::Base
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :labels, :dependent => :destroy
+  has_many :prohibited_words, :dependent => :destroy
 end
