c43e7db55d4fb34257e64cdfac2549fdf9b0984d
pastore27 <hirataku1988@gmail.com>
2016-05-25 00:21:17 +0900

    fix

--- lib/helper/amazon_ecs.rb
+++ lib/helper/amazon_ecs.rb
@@ -351,7 +351,8 @@ module Helper::AmazonEcs
     csv_header = %w/ path name code sub-code original-price price sale-price options headline caption abstract explanation additional1 additional2 additional3 jan/
     # テンプレートファイルを開く
     caption_erb = ''
-    if (user_id == '6' || user_id == '2') then
+    puts user_id
+    if (user_id == 6 || user_id == 2) then
       caption_erb = Rails.root.join('app/views/template/caption_for_yahuoku.html.erb').read
     else
       caption_erb = Rails.root.join('app/views/template/caption.html.erb').read
